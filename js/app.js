cf16b3184fc8a5623be7ed41c9d2cf362aeac9e0
Carlos Alberto <carlosescobedomoreno@gmail.com>
2021-07-17 23:29:29 -0500

    Funcion añadida, agregar citas al indexedDB y mostrarlas en el HTML

--- js/app.js
+++ js/app.js
@@ -1,24 +1,127 @@
 const formulario = document.querySelector('#formulario');
 const tituloCitas = document.querySelector('#titulo-citas');
 const contenedorCitas = document.querySelector('#contenedor-citas');
+const titulo = document.querySelector('#titulo');
+
+let DB;
+let contadorCitas = 0;
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    formulario.addEventListener('submit', agregarCita);
+    // Inicia la conexion con la base de datos
+    crearDB();
+
+    // Mostrar el HTML
+    if(window.indexedDB.open('crm', 1)) {
+        setTimeout(() => {
+            obtenerClientes();
+        }, 100);
+    }
+
+    // Valida los datos del formulario 
+    formulario.addEventListener('submit', validarCita);
 });
 
-function agregarCita(e) {
+function validarCita(e) {
     e.preventDefault();
 
-    const nombreInput = document.querySelector('#nombre').value;
-    const tipoInput = document.querySelector('#tipo').value;
-    const telefonoInput = document.querySelector('#telefono').value;
-    const emailInput = document.querySelector('#email').value;
-    const descripcionInput = document.querySelector('#descripcion').value;
+    const nombre = document.querySelector('#nombre').value;
+    const tipo = document.querySelector('#tipo').value;
+    const telefono = document.querySelector('#telefono').value;
+    const email = document.querySelector('#email').value;
+    const descripcion = document.querySelector('#descripcion').value;
     
-    if( nombreInput === '' || tipoInput === '' || telefonoInput === '' || emailInput === '' || descripcionInput === '' ) {
+    if( nombre === '' || tipo === '' || telefono === '' || email === '' || descripcion === '' ) {
         imprimirAlerta('Todos los campos son obligatorios', 'error');
 
         return;
+    }
+
+    const cita = {
+        nombre, 
+        tipo,
+        telefono,
+        email,
+        descripcion
+    }
+
+    cita.id = Date.now();
+
+    agregarCita(cita);
+
+    formulario.reset();
+
+    obtenerClientes();
+}
+
+function agregarCita(cita) {
+    const transaction = DB.transaction(['crm'], 'readwrite');
+    const objectStore = transaction.objectStore('crm');
+
+    objectStore.add(cita);
+
+    transaction.onerror = function() {
+        imprimirAlerta('Hubo un error', 'error');
+    } 
+
+    transaction.oncomplete = function() {
+        imprimirAlerta('Cita generada correctamente');
+    }
+}
+
+function obtenerClientes() {
+    // Se reabre la conexion a la base de datos
+    const abrirConexion = window.indexedDB.open('crm', 1);
+
+    abrirConexion.onerror = function() {
+        console.log('Hubo un error');
+    }
+
+    abrirConexion.onsuccess = function() {
+        DB = abrirConexion.result; // Se inserta el resultado a la variable
+
+        const objectStore = DB.transaction('crm').objectStore('crm'); // abrimos el objectstore
+
+        objectStore.openCursor().onsuccess = function(e) {
+            const cursor = e.target.result;
+
+            if(cursor) {
+                const { id, nombre, tipo, telefono, email, descripcion } = cursor.value;
+
+                contenedorCitas.innerHTML += `
+                    <div class="cita">
+                    <p class="title-cita">Servicio: ${tipo}</p>
+                    <div class="dato">
+                        <p class="f-n">Nombre:</p> 
+                        <p>${nombre}</p>
+                    </div>
+                    <div class="dato">
+                        <p class="f-n">Teléfono:</p> 
+                        <p>${telefono}</p>
+                    </div>
+                    <div class="dato">
+                        <p class="f-n">Correo:</p> 
+                        <p>${email}</p>
+                    </div>
+                    <div class="dato">
+                        <p class="f-n" >Descripción:</p> 
+                        <p>${descripcion}</p>
+                    </div>
+                    <div class="botones">
+                        <button class="btn-b btn">Editar </button>
+                        <button class="btn-r btn">Borrar</button>
+                    </div>
+                `;
+
+                contadorCitas++;
+
+                cursor.continue();
+            }
+
+            if(contadorCitas > 0) {
+                titulo.innerText = 'Administrar Citas';
+            }
+        }
     }
 }
 
